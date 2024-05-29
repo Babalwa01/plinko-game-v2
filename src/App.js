99ef4f0e8991de477811e424a0d072355720e443
Babalwa01 <babalwambolekwa93@gmail.com>
2024-05-29 21:30:46 +0200

    adding game sprites using resource loaded

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import * as PIXI from "pixi.js";
+import { Loader } from "./Loader";
 
 export class App {
-
     run() {
 
+        // create canvas
         this.app = new PIXI.Application({
             resizeTo: document.getElementById("canvas-container"),
             antialias: true
         });
-
         document.getElementById("canvas-container").appendChild(this.app.view);
 
-        console.log("canvas", this.app.view)
+        // load sprites
+        this.loader = new Loader(this.app.loader);
+        this.loader.preload().then(() => {
+            this.start();
+        });
+    }
+
+    // method to start game after resources have been loaded
+    start() {
+        console.log("the game started")
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import "./styles/main.scss";
 import { App } from "./App";
 import casino from "./sprites/casino.png";
 
+document.getElementById("logo").src = casino;
+
 document.addEventListener("DOMContentLoaded", (event) => {
-    document.getElementById("logo").src = casino;
 
     const app = new App();
     app.run();
